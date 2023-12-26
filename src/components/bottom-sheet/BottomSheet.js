3d5d0811dc7324f0f0308640e79fbd2691aa3b30
hassan.piou <piouhassan@gmail.com>
2023-12-26 17:08:42 +0000

    update

--- src/components/bottom-sheet/BottomSheet.js
+++ src/components/bottom-sheet/BottomSheet.js
@@ -1,56 +1,53 @@
-import React, { useEffect } from 'react';
+import { LitElement, html } from 'lit';
 
-const BottomSheet = ({ open, setOpen, children }) => {
-    useEffect(() => {
-        const dragIcon = document.querySelector(".drag-icon");
-        const sheetContent = document.querySelector(".content");
+ class BottomSheet extends LitElement {
 
-        let isDragging = false;
-        let startY = 0;
+     static properties = {
+         isOpen: { type: Boolean },
+     };
 
-        const handleMouseDown = (e) => {
-            isDragging = true;
-            startY = e.clientY;
-        };
+     constructor() {
+         super();
+         this.isOpen = true;
+     }
+
+
+
+     handleClose() {
+        this.dispatchEvent(new CustomEvent('onClose'));
+    }
+
+
+    handleMouseDown(e) {
+        const startY = e.clientY;
 
         const handleMouseMove = (e) => {
-            if (isDragging) {
-                const newHeight = (e.pageY / window.innerHeight) * 100;
-                sheetContent.style.height = `${newHeight}vh`;
-            }
+            const delta = startY - e.clientY;
+            const newHeight = Math.max(50, Math.min(100, 50 + delta / window.innerHeight * 100));
+            this.style.height = `${newHeight}vh`;
         };
 
         const handleMouseUp = () => {
-            isDragging = false;
-        };
-
-        dragIcon.addEventListener('mousedown', handleMouseDown);
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', handleMouseUp);
-
-        return () => {
-            dragIcon.removeEventListener('mousedown', handleMouseDown);
             document.removeEventListener('mousemove', handleMouseMove);
             document.removeEventListener('mouseup', handleMouseUp);
         };
-    }, []);
 
-    return (
-        <div className={`bottom-sheet ${open && "show"}`}>
-            <div className="sheet-overlay" onClick={() => setOpen(false)}></div>
-            <div className="content">
-                <div className="header">
-                    <div className="drag-icon" >
-                        <span></span>
-                    </div>
-                </div>
-                <div className="body">
-                    <h2>Bottom Sheet Modal</h2>
-                    {children}
-                </div>
-            </div>
-        </div>
-    );
-};
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+    }
 
-export default BottomSheet;
+
+    render() {
+        return this.isOpen
+            ? html`
+              <div class=${`bottom-sheet ${this.open ? 'show' : ''}`}>
+                <div class="sheet-overlay" @click=${this.handleClose}></div>
+                <div class="content" @mousedown=${this.handleMouseDown}>
+                  <slot #content></slot>
+                </div>
+              </div>
+    ` : html``
+    }
+}
+
+customElements.define('bottom-sheet', BottomSheet);
